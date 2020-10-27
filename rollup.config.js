ebe0acdcffa8a9f8890eb019af93b1237fe61fbe
opera <opera@operadeMacBook-Pro.local>
2020-10-27 16:08:57 +0800

    完善

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,34 @@
 import { terser } from 'rollup-plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+
 export default {
-    input: './src/sqlFormatter.js',
-    external: ['lodash'],
-    // "build:amd": "rollup src/sqlFormatter.js -f amd -o ./dist/sqlFormatter.amd.js",
-    // "build:cjs": "rollup src/sqlFormatter.js -f cjs -o ./dist/sqlFormatter.cjs.js",
-    // "build:es": "rollup src/sqlFormatter.js -f es -o ./dist/sqlFormatter.es.js",
-    // "build:iife": "rollup src/sqlFormatter.js -f iife -n result -o ./dist/sqlFormatter.iife.js",
-    // "build:umd": "rollup src/sqlFormatter.js -f umd -n result -o ./dist/sqlFormatter.umd.js",
-    // "build:all": "npm run build:amd && npm run build:cjs && npm run build:es && npm run build:iife && npm run build:umd"
+    input: './src/sql-formatter.js',
     output: [
         {
-            file: './dist/sql-formatter.umd.js',
+            file: 'dist/sql-formatter.umd.js',
+            name: 'sqlFormatter',
             format: 'umd',
-            name: 'sqlFormatter',
+            globals: {
+                lodash: 'lodash',
+            },
         },
         {
-            file: './dist/sql-formatter.cjs.js',
+            file: 'dist/sql-formatter.esm.js',
+            format: 'esm',
+        },
+        {
+            file: 'dist/sql-formatter.cjs.js',
             format: 'cjs',
-            name: 'sqlFormatter',
-        },
-        {
-            file: './dist/sql-formatter.es.js',
-            format: 'es',
-            name: 'sqlFormatter',
-        },
-        // {
-        //     file: './dist/sql-formatter.iife.js',
-        //     format: 'iife',
-        //     name: 'sqlFormatter',external:["lodash"],
-        // },
-        {
-            file: './dist/sql-formatter.amd.js',
-            format: 'amd',
-            name: 'sqlFormatter',
         },
     ],
     plugins: [
-        terser(),
+        nodeResolve(),
+        commonjs({
+            include: 'node_modules/**',
+        }),
+        terser(), //开启压缩格式
         babel({
             exclude: 'node_modules/**',
         }),
